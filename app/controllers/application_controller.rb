3ef4cb18292bc91270a8aab2185dac415898ab34
kenjikatooo <k.524.10nis@gmail.com>
2018-03-30 20:54:34 +0900

    change hello action word

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html:"1番の下手くそでいよう"
+    render html:"1番の下手くそでいような"
   end
 end
